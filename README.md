a43c47a8f78f24003f82983403cf5564f1c0e7a2
Phillip <phillipardrey@live.com>
2023-08-10 14:03:04 -0700

    Adjusted range max.
    As it is not necessary to test for factors beyond the square root of a given number range_max has been set to round up from the floating point value of a given number to an integer.

--- README.md
+++ README.md
@@ -2,15 +2,7 @@
 
 Here is a basic example of optimized conditional logic.
 
-The main.cpp is a complete program with in-depth documentation and simple explanations of how I implemented optimized conditional logic saving substantial computation in essentially two logical expressions condensing a common beginners method for testing prime numbers.
-
-
-==> Crash course for the curious before taking a closer look at process logic.
-Conditional expressions must evaluate as true for logical statements to be
-executed within a program. 
+The main.cpp is a complete program with in-depth documentation and simple explanations of how I implemented optimized conditional logic saving substantial computation in essentially two logical expressions condensing a method for testing prime numbers.
 
 The following screenshot is a live code example with detailed explanations in the comments. 
-
-![optimized conditional logic FindPrime](https://user-images.githubusercontent.com/61360844/228709241-efc76360-d360-4067-ad5e-3acbfc791819.png)
-
 
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ Program:FindPrime
 Author:Phillip McCullough
 Date:1/10/2022
 C/Cpp:11+ ANSI Standard
+last Updated:8/10/2023
 
 This program is a basic example of optimized conditional logic. Using the
 task of determining if a number is prime I express the meaning and value of
@@ -10,7 +11,8 @@ optimized conditional logic processing.
 -------10--------20--------30--------40--------50--------60--------70--------80
 */
 #include <iostream>
-#include<cctype>
+#include <cctype>
+#include <cmath>
 #include <string>
 using namespace std;
 
@@ -45,7 +47,7 @@ int main()
 		check_another = toupper(check_another[0]);
 
 	} while (check_another != "N");
-
+		
 	return 0;
 }
 
@@ -53,20 +55,23 @@ int main()
 
 bool findPrime(int check_number)
 {
-	int range_max = check_number / 2;
+	int range_max = ceil(sqrt(check_number));
 
 	/*
-	Crash course for the curious before taking a closer look at process logic.
-	Conditional expressions must evaluate as true for logical statements to be
-	executed within a program.
+	range_max has been set to the to the square root of check_number rounded up 
+	to an integer.
+
+	Important, crash course for the curious before taking a closer look at process
+	logic. Conditional expressions must evaluate as true for logical statements to
+	be executed within a program.
 	*/
 
-	// The line below is the one we want to examine most.
 	if (check_number % 2 == 0 && check_number != 2 || check_number <= 1) 
 		return false;
+
 	/*
 	Let's start with the first set of conditions.
-	(check_number % 2 == 0 && check_number != 2) This translates to, does
+	(check_number % 2 == 0 && check_number != 2) This translates to: does
 	check_number divided by two have a remainder and is check_number not equal
 	to two.
 
@@ -81,7 +86,7 @@ bool findPrime(int check_number)
 	one is controversially excluded from prime numbers. Rather than creating a
 	separate statement using operator precedence with the logical or (||) operator
 	only in the case of odd numbers check_number will be compared via the comparison
-	operator (<=) to exclude one and negitive numbers as prime.
+	operator (<=) to exclude one and negative numbers as prime.
 
 	In conclusion on one line within an orchestrated logical expression all even
 	numbers have been eliminated except for the number two. Also, using operator
@@ -89,13 +94,18 @@ bool findPrime(int check_number)
 	numbers the right value is not examined saving many operations by using
 	(optimized order processing conditional logic).
 	
-
 	With even numbers eliminated the following loop starts testing odd numbers
-	as possibly prime starting with the first non-prime odd number: nine.
+	as possibly prime starting with the first non-prime odd number: 9.
 	*/
-	for (int i = 3; i < range_max; i += 2)
+
+	for (int i = 3; i <= range_max; i += 2)
 		if (check_number % i == 0)
 			return false;
-     // True is returned as check_number is declared to be a prime number.
+
+	/*
+	True is returned if check_number has no factors up to the square root of
+	the given number.
+	*/
+
 	return true;
 }
